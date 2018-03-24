5ef1732022b22279345b87f3f96e2fd18d15fa82
Kay <i@v2bv.net>
2018-03-24 14:14:26 -0700

    Add pubkey update scripts.

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ and contributors, if you would like your public key(s) added to the record...
 
 - Make a code/artwork/creativity contribution to any of our projects.
 - Contact Mingcong Bai <[jeffbai@aosc.io](mailto:jeffbai@aosc.io)>.
+
+Deploy pubkey update script on build bot
+----------------------------------------
+
+`sh -c "$(curl -fsSL https://raw.githubusercontent.com/AOSC-Dev/dev-pubkeys/master/install.sh)"`
---
+++ script/update.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e;
+
+PUBKEY_REPO=https://github.com/AOSC-Dev/dev-pubkeys.git;
+BRANCH=master;
+LOCAL_DIR=pubkeys;
+SSH_DIR=$HOME/.ssh;
+AUTHORIZED_KEYS=authorized_keys;
+TEMP_FILE=temp_keys;
+
+# Pull the pubkeys
+if [ ! -d $LOCAL_DIR ]; then # If the directory does not exist.
+	git clone $PUBKEY_REPO -b $BRANCH $LOCAL_DIR;
+elif [ ! -d $LOCAL_DIR/.git ]; then # If the direcotry isn't a git repo.
+	rm -Rf $LOCAL_DIR;
+	git clone $PUBKEY_REPO -B $BRANCH $LOCAL_DIR;
+fi
+pushd $LOCAL_DIR;
+git pull;
+git checkout master;
+popd;
+
+# Update pubkeys
+cat $SSH_DIR/$AUTHORIZED_KEYS > $TEMP_FILE;
+cat $LOCAL_DIR/authorized_keys >> $TEMP_FILE;
+awk '!seen[$0]++' $TEMP_FILE > $SSH_DIR/$AUTHORIZED_KEYS; # Remove duplicated keys
+rm $TEMP_FILE; # Remove temp file
+
+# Fix permission
+chmod 700 $SSH_DIR;
+chmod 600 $SSH_DIR/$AUTHORIZED_KEYS;
+chmod go-w ~;
